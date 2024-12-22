7a97954a871df6509cf68e6f7b8df190544a6e21
vladseme2012 <vlad.seme2012@yandex.ru>
2024-12-22 13:26:01 +0300

    added file Theme.tsx for colors and media variables

---
+++ src/styles/Theme.tsx
@@ -0,0 +1,16 @@
+export const theme = {
+    colors: {
+        primary: '#7562E0',
+        secondary: '#181824',
+        colorText: '#F5F5F5',
+        backgroundColor: '#BDBDBD',
+        firstGradientColor: 'rgba(24, 24, 36, 0.25)',
+        secondGradientColor: 'rgba(117, 98, 224, 0.5)',
+        backgroundColorField: '#31313F',
+    },
+    media: {
+        tablet: 'screen and (max-width: 768px)',
+        mobile: 'screen and (max-width: 576px)',
+        desktop: 'screen and (min-width: 1140px)',
+    },
+};
