1f46350746aef82eb62d13be8ad09248e673b64f
vladseme2012 <vlad.seme2012@yandex.ru>
2024-12-22 16:56:34 +0300

    created file Logo_Styles.tsx for Logo

---
+++ src/components/logo/Logo_Styles.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import { theme } from '../../styles/Theme';
+
+export const LogoTitle = styled.h3`
+    color: ${theme.colors.primary};
+    text-transform: capitalize;
+`;
+
+export const S = {
+    LogoTitle,
+};
